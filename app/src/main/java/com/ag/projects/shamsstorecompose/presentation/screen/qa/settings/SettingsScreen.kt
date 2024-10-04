a438d73b99a0e7366ae27612b4e616d14e0bbe34
Paint <AHMED00AHMED00GAMAL@gmail.com>
2024-10-04 19:28:28 +0200

    redesign the setting screen, contain a row for a language for selected language

--- app/src/main/java/com/ag/projects/shamsstorecompose/presentation/screen/qa/settings/SettingsScreen.kt
+++ app/src/main/java/com/ag/projects/shamsstorecompose/presentation/screen/qa/settings/SettingsScreen.kt
@@ -1,11 +1,13 @@
 package com.ag.projects.shamsstorecompose.presentation.screen.qa.settings
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
@@ -14,11 +16,7 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.ModalBottomSheetLayout
 import androidx.compose.material.ModalBottomSheetValue
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Language
 import androidx.compose.material.rememberModalBottomSheetState
-import androidx.compose.material3.Button
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -26,12 +24,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.ag.projects.shamsstorecompose.R
@@ -91,30 +89,30 @@ fun SettingsScreen(
                 modifier = Modifier
                     .fillMaxSize()
                     .padding(16.dp),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Text(
-                    text = stringResource(
-                        R.string.current_language,
-                        if (selectedLanguage == "en")
-                            stringResource(id = R.string.english)
-                        else stringResource(
-                            id = R.string.arabic
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(Color.White)
+                        .clickable {
+                            coroutineScope.launch { sheetState.show() }
+                        }
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_language),
+                        contentDescription = stringResource(
+                            id = R.string.language
                         )
                     )
-                )
-                Spacer(modifier = Modifier.height(16.dp))
-
-                Button(onClick = {
-                    coroutineScope.launch { sheetState.show() }
-                }) {
-                    Icon(
-                        imageVector = Icons.Default.Language,
-                        contentDescription = stringResource(R.string.change_language)
+                    Spacer(modifier = Modifier.width(5.dp))
+                    Text(
+                        modifier =Modifier.fillMaxWidth(0.7f),
+                        text = stringResource(id = R.string.language),
                     )
-                    Spacer(modifier = Modifier.width(8.dp))
-                    Text(text = stringResource(R.string.change_language))
+                    Text(
+                        text = stringResource(id = R.string.language),
+                        fontWeight = FontWeight.Bold,
+                    )
                 }
             }
         }
