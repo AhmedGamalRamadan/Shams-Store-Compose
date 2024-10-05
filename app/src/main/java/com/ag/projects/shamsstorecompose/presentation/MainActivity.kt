e09d9788a6ea906a4d649c9f714d06603e71ab98
Paint <AHMED00AHMED00GAMAL@gmail.com>
2024-10-05 20:58:58 +0200

    get the latitude and longitude

--- app/src/main/java/com/ag/projects/shamsstorecompose/presentation/MainActivity.kt
+++ app/src/main/java/com/ag/projects/shamsstorecompose/presentation/MainActivity.kt
@@ -1,21 +1,76 @@
 package com.ag.projects.shamsstorecompose.presentation
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import com.ag.projects.shamsstorecompose.presentation.navigation.Navigation
 import com.ag.projects.shamsstorecompose.presentation.ui.theme.ShamsStoreComposeTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 
 @AndroidEntryPoint
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(), LocationListener {
+
+    private lateinit var locationManager: LocationManager
+    private val locationPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) {}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ShamsStoreComposeTheme {
+                locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
                 Navigation()
+
+                if (isLocationPermissionGranted()) {
+                    if (ActivityCompat.checkSelfPermission(
+                            this,
+                            Manifest.permission.ACCESS_FINE_LOCATION
+                        ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                            this,
+                            Manifest.permission.ACCESS_COARSE_LOCATION
+                        ) != PackageManager.PERMISSION_GRANTED
+                    ) {
+                       return@ShamsStoreComposeTheme
+
+                    }
+
+                    locationManager.requestLocationUpdates(
+                        LocationManager.GPS_PROVIDER,
+                        5000L,
+                        10f,
+                        this
+                    )
+                }
+
             }
         }
+    }
+
+
+    private fun isLocationPermissionGranted(): Boolean {
+        return ActivityCompat.checkSelfPermission(
+            this,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+            this,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onLocationChanged(location: Location) {
+        val latitude = location.latitude
+        val longitude = location.longitude
+        Log.d("Location is ", "$latitude .. ... $longitude")
     }
 }
