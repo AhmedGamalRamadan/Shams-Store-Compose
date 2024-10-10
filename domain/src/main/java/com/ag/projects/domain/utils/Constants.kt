398c80fbb8f4da3444406ff0ba2a4aeb4744c5e4
Paint <AHMED00AHMED00GAMAL@gmail.com>
2024-10-10 20:41:47 +0200

    add endpoint for auth

--- domain/src/main/java/com/ag/projects/domain/utils/Constants.kt
+++ domain/src/main/java/com/ag/projects/domain/utils/Constants.kt
@@ -2,9 +2,12 @@ package com.ag.projects.domain.utils
 
 object Constants {
 
+    /*
+    Products
+     */
     const val BASE_URL = "https://shams-pharmacis.products.aait-d.com/api/app/"
-    const val HOME ="home"
-    const val CATEGORIES ="categories"
+    const val HOME = "home"
+    const val CATEGORIES = "categories"
     const val LAT = 31.022855
     const val LNG = 31.3871883
     const val MOST_POPULAR_PRODUCTS = "most_popular_products"
@@ -15,9 +18,34 @@ object Constants {
     const val FIRST_BANNER_SLIDERS = "banner_sliders"
     const val MIDDLE_BANNER_SLIDER = "middle_slider"
 
-    const val POLICY ="policy"
-    const val TERMS ="terms"
+    /*
+    App Info
+     */
+    const val POLICY = "policy"
+    const val TERMS = "terms"
     const val ABOUT = "about"
-    const val FAQ ="FAQs"
+    const val FAQ = "FAQs"
     const val CONTACT = "contact"
+
+    const val GET_COUNTRIES = "countries"
+
+    /*
+    Auth
+     */
+    const val REGISTER = "register"
+    const val LOGIN = "login"
+    const val VERIFY_PHONE = "verify"
+    const val RESEND_CODE = "resend_code"
+    const val EDIT_NUMBER = "edit_number"
+    const val EDIT_PROFILE = "profile"
+    const val LOGOUT = "logout"
+    const val CHECK_CODE = "check-code"
+    const val FORGET_PASSWORD = "send-code"
+    const val RESET_PASSWORD = "reset-password"
+
+    const val COUNTRY_ID = "country_id"
+    const val PHONE = "phone"
+    const val DEVICE_TOKE = "device_token"
+    const val GUEST_TOKEN = "guest_token"
+    const val TYPE = "type"
 }
