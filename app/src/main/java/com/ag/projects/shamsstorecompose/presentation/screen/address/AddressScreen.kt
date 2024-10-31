ba133ebc41d9a937e3adc90146ed2048edceab93
Paint <AHMED00AHMED00GAMAL@gmail.com>
2024-10-31 20:27:54 +0200

    design address screen.

--- app/src/main/java/com/ag/projects/shamsstorecompose/presentation/screen/address/AddressScreen.kt
+++ app/src/main/java/com/ag/projects/shamsstorecompose/presentation/screen/address/AddressScreen.kt
@@ -1,13 +1,18 @@
 package com.ag.projects.shamsstorecompose.presentation.screen.address
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -18,15 +23,22 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.ag.projects.data.local.SharedPreferencesManager
 import com.ag.projects.shamsstorecompose.R
 import com.ag.projects.shamsstorecompose.presentation.components.CommonHeader
+import com.ag.projects.shamsstorecompose.presentation.ui.theme.Green
+import com.ag.projects.shamsstorecompose.presentation.ui.theme.Grey
+import com.ag.projects.shamsstorecompose.presentation.ui.theme.LightGreen
 import com.ag.projects.shamsstorecompose.utils.Result
 
 @Composable
@@ -43,9 +55,9 @@ fun AddressScreen(
     val context = LocalContext.current
     val sharedPrefManager = SharedPreferencesManager(context)
 
-    LaunchedEffect(key1 = Unit){
+    LaunchedEffect(key1 = Unit) {
         viewModel.getAddresses(
-            auth=""
+            auth = ""
         )
     }
 
@@ -71,25 +83,62 @@ fun AddressScreen(
             is Result.Success -> {
                 val userAddresses = (addresses as Result.Success).data.data
                 if (userAddresses.isEmpty()) {
-                    Box(
+                    Column(
                         modifier = Modifier.fillMaxSize(),
-                        contentAlignment = Alignment.Center
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
                     ) {
                         Image(
                             painter = painterResource(id = R.drawable.ic_no_location),
                             contentDescription = stringResource(id = R.string.no_data_available)
                         )
+                        Text(
+                            text = stringResource(id = R.string.there_is_not_any_addresses_yet),
+                            textAlign = TextAlign.Center,
+                            color = Grey,
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 26.sp
+                        )
+                        Spacer(modifier = Modifier.height(15.dp))
+
+                        Text(
+                            text = stringResource(id = R.string.you_do_not_add_address),
+                            textAlign = TextAlign.Center,
+                            color = Color.Black,
+                            fontSize = 15.sp
+                        )
                     }
                 } else {
-                    LazyColumn(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(start = 14.dp, end = 14.dp)
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
                     ) {
-                        items(userAddresses) {
-                            Text(text = it.country.toString())
+                        LazyColumn(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(start = 14.dp, end = 14.dp)
+                        ) {
+                            items(userAddresses) {
+                                Text(text = it.country.toString())
+                            }
+                        }
+
+                        Button(
+                            onClick = { },
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .align(Alignment.BottomCenter)
+                                .padding(start = 14.dp, end = 14.dp),
+                            colors = ButtonDefaults.buttonColors(
+                                backgroundColor = LightGreen
+                            ),
+                        ) {
+                            Text(
+                                text = stringResource(id = R.string.add_new_address),
+                                color = Color.White
+                            )
                         }
                     }
+
                 }
             }
         }
